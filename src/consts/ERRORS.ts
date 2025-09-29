2a83a6bbf10da69bcc793ec416d5fd78c41d465e
OosukeRen <oosuke_ren@abv.bg>
2025-09-29 03:04:18 +0300

    improved the Rule class core concept/logic and also the way Errors are handled

--- src/consts/ERRORS.ts
+++ src/consts/ERRORS.ts
@@ -1,7 +1,32 @@
+const MandatoryProperty = (property: string, target: string) => {
+    return `Field [${property}] is mandatory ${target ? "for " + target : ""}!`;
+}
+
+
+const MPR = (property: string)  => {
+    return MandatoryProperty(property, "rules");
+};
+
+
 export default {
     RULES: {
         CONDITION: {
-            MISSING: "You cannot create a rule that doesn't have a condition",
+            MISSING: MPR("condition"),
+        },
+        NAME: {
+            MISSING: MPR("name"),
+        },
+        PRIORITY: {
+            MISSING: MPR("priority"),
+        },
+        TABLE: {
+            MISSING: MPR("table"),
+        },
+        WHEN: {
+            MISSING: MPR("when")
+        },
+        EXECUTE: {
+            DEBUG: MPR
         }
     }
 }
--- src/rules.ts
+++ src/rules.ts
@@ -6,31 +6,109 @@ declare global {
   class Rule {
     name: string;
     table: string;
-    condition: Function;
+    condition: RuleFunction;
     priority: number;
-    execute: Function;
+    when: When;
+    description: string;
 
-    constructor(args: {
-      name: string;
-      table: string;
-      condition: Function;
-      priority?: number;
-    });
+    constructor(constr: RuleConstructor);
+  }
+
+  function Log(property: any): void;
+}
+
+declare type When = {
+    CREATE?: boolean;
+    READ?: boolean;
+    UPDATE?: boolean;
+    DELETE?: boolean;
+};
+
+declare type RuleFunction = (record: any, previous: any) => Promise<boolean>;
+
+declare type RuleConstructor = {
+    name: string;
+    table: string;
+    condition: RuleFunction;
+    when: When;
+}
+
+class BaseRuleError extends Error {
+  cause?: unknown;
+  constructor(_rule: Rule, message: string, cause?: unknown) {
+    message = `${RuleErrorWrapper(_rule)}${message}`
+    super(message);
+
+
+    // this.name = "RuleError";
+    this.cause = cause;
+
+    Object.setPrototypeOf(this, Error.prototype);
+    if ((Error as any).captureStackTrace) {
+      (Error as any).captureStackTrace(this, new.target);
+    }
   }
 }
+
+const RuleError = (rule: Rule, message: string, cause?: unknown) => {
+    throw new BaseRuleError(rule, message, cause);
+};
+
+const RuleErrorWrapper = (_rule: Rule) =>  {
+    return `\tRule Name: ${_rule.name}\n`;
+}
+
 
 class Rule {
     name: string;
     table: string;
-    condition: Function;
+    condition: RuleFunction;
     priority: number;
-    execute: Function;
+    #execute: RuleFunction;
+    when: When;
+    description: string;
 
-    constructor({name, table, condition, priority}) {
-        if(!condition) {
-            throw new Error(errors.RULES.CONDITION.MISSING);
+    constructor(constr: RuleConstructor) {
+        let {name, table, condition, when} = constr;
+        if(!name) {
+            RuleError(this, errors.RULES.NAME.MISSING);
         }
+        
+        this.name = name;
+
+        if(!condition) {
+            RuleError(this, errors.RULES.CONDITION.MISSING);
+        }
+
+        if(!table) {
+            RuleError(this, errors.RULES.TABLE.MISSING);
+        }
+
+        if(!when) {
+            RuleError(this, errors.RULES.WHEN.MISSING);
+        }
+
+        this.when = when;
+        this.condition = condition;
+        this.table = table;
+
+        Object.defineProperty(this, "execute", {
+            set(_execute) {
+                this.#execute = _execute;
+            },
+            get() {
+                return async function (record: any, previous: any) {
+                    try {
+                        await this.#execute(record, previous);
+                    } catch(e) {
+                        (Log || console.log)(e);
+                    }
+                }
+            }
+        })
     }
+
+
 }
 
 // Install globally at runtime
